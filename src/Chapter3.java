3610ffd3d950a648167b0709dac17de5d5a2f97d
Barry Mikulas <salukim@gmail.com>
2021-03-25 18:20:48 -0500

    chapter 3 milestone 1

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
 
 public class Chapter3 {
-    // Declare the Chapter 3 Milestone 1 method here
+    // Chapter 3 Milestone 1
     public void milestone1(int num1, int num2) {
-        
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        for (int i = num1 + 1; i < num2; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            if (i % num1 == 0 && i != 0) {
+                break;
+            }
+        }
     }
-    
+
     // Declare the Chapter 3 Milestone 2 method here
     public void milestone2(Scanner scanner) {
         // ask the user to enter a bunch of numbers
